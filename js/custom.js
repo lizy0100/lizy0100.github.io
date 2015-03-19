62eb617446a2fbcad69b145cd0f635edab6ae812
lizy0100 <lizy0100@gmail.com>
2015-03-19 22:27:32 +0800

    add commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
+ * Author:HtmlCoder
+ * Author URI:http://www.htmlcoder.me
+ * Version:1.0.0
+ * Created:November 2014
+ * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
+ * File Description: Place here your custom scripts
+ */
+$(window).load(function() { 
+	//Preloader 
+	$('#status').delay(300).fadeOut(); 
+	$('#preloader').delay(300).fadeOut('slow');
+	$('body').delay(550).css({'overflow':'visible'});
+})
